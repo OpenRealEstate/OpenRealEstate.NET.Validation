c05b69d84ddd176907fa8a3b25f5e13030d19d7c
Pure Krome <pure.krome@world-domination.com.au>
2020-01-15 21:25:00 +1100

    :lipstick:

--- src/OpenRealEstate.Validation/Extensions/IRuleBuilderOptionsExtensions.cs
+++ src/OpenRealEstate.Validation/Extensions/IRuleBuilderOptionsExtensions.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using FluentValidation;
 using OpenRealEstate.Core;
 
@@ -8,10 +5,6 @@ namespace OpenRealEstate.Validation.Extensions
 {
     internal static class IRuleBuilderOptionsExtensions
     {
-        // public static IRuleBuilderOptions<T, TProperty> When<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, 
-        //      Func<T, bool> predicate, 
-        //      ApplyConditionTo applyConditionTo = ApplyConditionTo.AllValidators);
-
         internal static IRuleBuilderOptions<T, TProperty> WhenStatusTypeIsAvailableOrUnknown<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule) where T : Listing
         {
             return rule.When(listing => listing.StatusType == StatusType.Available ||
